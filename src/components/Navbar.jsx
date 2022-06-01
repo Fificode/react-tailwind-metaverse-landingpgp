bfbe93b3a94a1efa6e00fc075c5570501e371b5c
Fificode <lamidifirdaws@gmail.com>
2022-06-01 23:29:15 +0100

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,8 @@ const handleClick = () => setNav(!nav);
     <div className='w-screen h-[80px] z-10 fixed my-5'>
         <div className="w-full h-full flex justify-between items-center">
 <div className='flex flex-row mx-2'>
-<img src={logo} alt="Logo" className='w-[48px] h-[48px] mx-1'/>
-<h1 className='text-5xl font-bold text-white'>Metaverse</h1>
+<img src={logo} alt="Logo" className='w-[38px] h-[38px] mx-1 sm:w-[48px]h-[48px]'/>
+<h1 className='text-3xl font-bold text-white sm:text-5xl'>Metaverse</h1>
 </div>
 <ul className='hidden lg:flex'>
 <li>Home</li>
@@ -21,22 +21,23 @@ const handleClick = () => setNav(!nav);
 </ul>
 <div className="hidden md:flex">
     <button className='px-3 text-xl font-normal text-white bg-transparent'>Sign In</button>
-    <button className='px-6 py-3 mx-3 rounded bg-blue-600 text-xl'>Sign Up</button>
+    <button className='px-6 py-2 mx-3 rounded-[10px] bg-blue-btn text-black text-xl'>Sign Up</button>
 </div>
 <div className="lg:hidden" onClick={handleClick}>
     {!nav ? <MenuIcon className='w-10 text-white m-2' /> : <XIcon className='w-10 text-white m-2'/>}
 </div>
 </div>
+
 <ul className={!nav ? 'hidden' : 'absolute w-full px-8'}>
 <li className='w-full'>Home</li>
 <li className='w-full'>How it works</li>
 <li className='w-full'>Event</li>
 <li className='w-full'>Market place</li>
-</ul>
-<div className={!nav ? 'hidden' : 'flex flex-col my-4'}>
-    <button className='px-3 text-xl font-normal text-white bg-transparent'>Sign In</button>
-    <button className='px-6 py-3 mx-3 rounded bg-blue-600 text-xl'>Sign Up</button>
+<div className= {!nav ? 'hidden' : 'flex flex-col'}>
+    <button className='px-3 text-xl font-normal text-white bg-transparent text-left py-4'>Sign In</button>
+    <button className='px-6 py-2 mx-3 rounded-[10px] bg-blue-btn text-xl text-black'>Sign Up</button>
 </div>
+</ul>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
+    colors: {
+'blue-black':'#02090F',
+'white': '#ffffff',
+'blue-btn': '#33A5FE',
+'black': '#000000',
+    },
     screens: {
       sm: '640px',
       md: '768px',
