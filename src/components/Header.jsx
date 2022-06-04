b2587ba846b02f12e98a105bfc3d97b0e26eebb1
Fificode <lamidifirdaws@gmail.com>
2022-06-05 00:05:09 +0100

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,29 +8,29 @@ import playbtn from '../assets/playbtn.png'
 const Header = () => {
   return (
     <div>
-    <div className='flex flex-row medium:flex-col'>
-      <div className="flex items-start justify-center flex-1 flex-col mx-[30px] mt-[40px] large:mt-[150px]">
+    <div className='flex flex-row xmedium:flex-col'>
+      <div className="flex items-start justify-center flex-1 flex-col mx-[15px] mt-[40px] large:mt-[150px] sm:mx-[30px]">
        <div className="flex flex-row"> 
-       <h1 className="text-white text-xl font-normal">Trendy & Amazing Features</h1>
-       <hr className='text-white w-[40px] mt-4 mx-3'/>
+       <h1 className="text-white text-[14px] font-normal msmaller:text-[17px] md:text-[20px]">Trendy & Amazing Features</h1>
+       <hr className='text-white w-[40px] mx-3 translate-y-4'/>
        <div className="flex flex-row mt-[10px]">
          <img src={greencircle} alt="Green circle" className='w-[12px] h-[12px]'/>
          <img src={orangecircle} alt="Orange circle" className='w-[12x] h-[12px] mx-2'/>
          <img src={bluecircle} alt="Blue circle" className='w-[12px] h-[12px]'/>
        </div>
        </div>
-       <h2 className='font-bold text-white text-[104px] tracking-[0.02em] capitalize leading-[120px] large:text-[85px] smaller:text-[60px]'>METAVERSE</h2>
-      <div className='flex flex-row'> <h2 className='font-bold text-white text-[104px] tracking-[0.02em] capitalize large:text-[85px] smaller:text-[60px]'>MA</h2>
-      <img src={letterd} alt="Capital Letter D" className=' smaller:w-[50px]h-[20px] inline w-[136px] h-[80px] translate-y-11'/>
-     <h2 className='font-bold text-white text-[104px] tracking-[0.02em] capitalize large:text-[85px] smaller:text-[60px]'>NESS</h2>
+       <h2 className='font-bold text-white tracking-[0.02em] capitalize text-[40px] leading-[80px] msmaller:text-[60px] md:text-[85px] md:leading-[120px] xlarge:text-[104px]'>METAVERSE</h2>
+      <div className='flex flex-row'> <h2 className='font-bold text-white text-[40px] tracking-[0.02em] capitalize msmaller:text-[60px] md:text-[85px] xlarge:text-[104px]'>MA</h2>
+      <img src={letterd} alt="Capital Letter D" className='w-[50px] h-[35px] translate-y-4 pl-[2px] inline mmsmall:w-[50px] mmsmall:h-[35px] mmsmall:translate-y-[13px] msmall:w-[60px] msmall:h-[35px] msmall:translate-y-[14px] md:w-[110px] md:h-[65px] md:translate-y-9 xlarge:w-[126px] xlarge:[100px] xlarge:translate-y-14'/>
+     <h2 className='font-bold text-white text-[40px] tracking-[0.02em] capitalize msmaller:text-[60px] md:text-[85px] xlarge:text-[104px]'>NESS</h2>
      </div>
-     <p className='text-white text-xl font-normal'>We provide the best service for your company products in real world and the Meta-world</p>
+     <p className='text-white text-[17px] font-normal md:[20px]'>We provide the best service for your company products in real world and the Meta-world</p>
      <div className="flex flex-row mt-[20px]">
-       <button className='text-black px-[40px] py-[15px] bg-blue-btn rounded-[10px] text-[20px] smaller:px-[20px]py-[5px]text-[13px]'>Get Started</button>
-       <div className='border-[1px] border-solid  border-white rounded-[50%] w-[50px] h-[50px] mx-[60px]'>
+       <button className='text-black bg-blue-btn rounded-[10px] px-[20px] py-[2px] text-[13px] msmaller:px-[30px] msmaller:py-[2px] msmaller:text-[14px] md:text-[20px] md:px-[40px] md:py-[15px]'>Get Started</button>
+       <div className='border-[1px] border-solid  border-white rounded-[50%] w-[50px] h-[50px] mx-[30px]'>
          <img src={playbtn} alt="Play button" className='translate-x-4 translate-y-3' />
        </div>
-       <p className='text-white font-[600] text-[20px]'>See Demo</p>
+       <p className='text-white font-[600] text-[14px] translate-y-3 msmaller:text-[17px] md:text-[20px]'>See Demo</p>
      </div>
       </div>
       <div className="flex justify-center items-center flex-1 mt-[40px] mx-[30px]">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,11 +7,11 @@ const [nav, setNav] = useState(false);
 const handleClick = () => setNav(!nav);
 
   return (
-    <div className='w-screen h-[80px] z-10 fixed top-0 bg-blue-black  py-[50px] pl-[10px] pr-[30px]'>
+    <div className='w-screen h-[80px] z-10 fixed top-0 bg-blue-black  py-[50px] pl-[10px] md:pr-[30px]'>
         <div className="w-full h-full flex justify-between items-center">
 <div className='flex flex-row'>
-<img src={logo} alt="Logo" className='small:w-[38px]h-[38px] mx-1 w-[48px] h-[48px]'/>
-<h1 className='small:text-3xl font-bold text-white tracking-[0.02em] text-5xl'>Metaverse</h1>
+<img src={logo} alt="Logo" className='w-[35px] h-[35px] mx-1 md:w-[48px] md:h-[48px]'/>
+<h1 className='text-3xl font-bold text-white tracking-[0.02em] md:text-5xl'>Metaverse</h1>
 </div>
 <ul className='flex large:hidden'>
 <li>Home</li>
@@ -23,12 +23,12 @@ const handleClick = () => setNav(!nav);
     <button className='pr-[15px] text-xl font-normal text-white bg-transparent'>Sign In</button>
     <button className='px-[30px] py-[10px] rounded-[10px] bg-blue-btn text-black text-xl'>Sign Up</button>
 </div>
-<div className="xlg:hidden" onClick={handleClick}>
+<div className="xlarge:hidden" onClick={handleClick}>
     {!nav ? <MenuIcon className='w-10 text-white m-2' /> : <XIcon className='w-10 text-white m-2'/>}
 </div>
 </div>
 
-<ul className={!nav ? 'hidden' : 'absolute w-full h-[400px] px-8 mt-[50px] bg-blue-black md:h-[300px]'}>
+<ul className={!nav ? 'hidden' : 'absolute w-full h-[400px] px-5 mt-[50px] bg-blue-black md:h-[280px]'}>
 <li className='w-full'>Home</li>
 <li className='w-full'>How it works</li>
 <li className='w-full'>Event</li>
